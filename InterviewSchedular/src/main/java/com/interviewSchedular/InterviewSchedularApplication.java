ffdd68f16f5e7e755779ecadc58788ca2aa38415
Geetu Rana <geeturana07@gmail.com>
2023-02-17 21:53:59 +0530

    Interview Schedular application initiated

---
+++ InterviewSchedular/src/main/java/com/interviewSchedular/InterviewSchedularApplication.java
@@ -0,0 +1,13 @@
+package com.interviewSchedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewSchedularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewSchedularApplication.class, args);
+	}
+
+}
---
+++ InterviewSchedular/src/test/java/com/interviewSchedular/InterviewSchedularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewSchedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewSchedularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

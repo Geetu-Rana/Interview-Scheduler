140e1708632814e1376d09c35344e2287b7f9ca9
Geetu Rana <geeturana07@gmail.com>
2023-02-18 23:26:45 +0530

    Interview Schedular application Some classes Added

---
+++ InterviewSchedular/src/main/java/com/interviewSchedular/Model/Interviewee.java
@@ -0,0 +1,25 @@
+package com.interviewSchedular.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Interviewee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long intervieweeId;
+	
+	private String intervieweeName;
+	
+	private String intervieweeDetails;
+	
+}
---
+++ InterviewSchedular/src/main/java/com/interviewSchedular/Model/Interviewer.java
@@ -0,0 +1,38 @@
+package com.interviewSchedular.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Interviewer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long interviewerId;
+	
+	@NotNull(message = "Interviewer Name can not be null")
+	@NotBlank(message = "Interviews name can not be Blank")
+	private String interviewerName;
+	
+	private String interviewDetails;
+	
+	private String jobRole;
+	
+	private String jobDescription;
+	
+	private String location;
+	
+	private Integer ctc;
+	
+	private String addDetails;
+	
+}
